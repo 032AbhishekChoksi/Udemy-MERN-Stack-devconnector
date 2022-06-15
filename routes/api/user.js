25871255426cfa9bd1f0e8f17b143a50434ae8f6
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-06-15 14:20:29 +0530

    Implementing JWT

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -52,9 +54,24 @@ router.post(
 
             await user.save();
 
-            // Return jsonwebtoken (Pending)
+            // Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user._id
+                }
+            };
 
-            res.send('User Registered');
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+
+            // res.send('User Registered');
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server Error');
