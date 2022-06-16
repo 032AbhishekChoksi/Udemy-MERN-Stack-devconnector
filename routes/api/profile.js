b6f59634c756f8045bf62114aa3d1f823eaaf89f
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-06-16 21:30:40 +0530

    Delete Profile & User

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -157,4 +157,24 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete Profile, user & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @todo - remove users posts
+
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User Deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
