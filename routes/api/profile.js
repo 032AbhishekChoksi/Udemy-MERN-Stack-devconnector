8ff601d261ac3f12395689636f9f29f4270a6dfe
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-06-17 10:26:00 +0530

    Delete Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -224,4 +224,29 @@ router.put(
         }
     }
 );
+
+// @route   Delete api/profile/experience/:exp_id
+// @desc    Delete Profile Experience
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const exp_id = req.params.exp_id;
+
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // Get remove index
+        const removeIndex = profile.experience
+            .map((item) => item.id)
+            .indexOf(exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
